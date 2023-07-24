7cc74992014b608b744b179a71a84d8470045ef5
Stephen Mbau <mbaustephen@gmail.com>
2023-07-24 13:19:17 +0300

    done assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const mainElement = document.getElementById('main');
+mainElement.remove();
+const newHeader = document.createElement('h1');
+newHeader.setAttribute('id','victory');
+const bodyName = document.querySelector('body');
+bodyName.appendChild(newHeader);
+document.getElementsByTagName("h1")[0].innerHTML="Stephen is the champion";
